aa5ebd50b3f5da5c09cd142f4167f2f29b3b188e
«¥£ ¤¨⮭ <aditon1800@gmail.com>
2019-05-18 15:51:52 +0300

    First commit

---
+++ lesson_1/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  "use strict";
+
+  var $slider = $('.js-slider');
+
+  $slider.slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+});
